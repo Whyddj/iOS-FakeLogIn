ee423ed709458afe1d9b08432785e68022e198d9
why <1420652550@qq.com>
2022-11-03 09:08:29 +0800

    稍加美化

--- ContentView.swift
+++ ContentView.swift
@@ -6,30 +6,88 @@
 //
 
 import SwiftUI
- 
+
 struct ContentView: View {
-    let trueaccount = "zhanghao"
+    let trueaccount = "Zhanghao"
     let truepassword = "mima"
     @State var account = ""
     @State var password = ""
+    @State var isClicked1: Bool = false
+    @State var isClicked2: Bool = false
     var body: some View {
         NavigationView() {
             VStack() {
-                TextField("Your Account", text: $account)
-                SecureField("Please Type in Your Password", text: $password )
-                NavigationLink(destination: account==trueaccount && password == truepassword ? Text("登录成功") : Text("登录失败")){
-                    Text("登录")
+                Image("image1")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 100)
+                    .cornerRadius(15)
+                    .shadow(radius: 10,y: 10)
+                    .padding(.vertical,80)
+                VStack(){
+                    HStack(){
+                        Image(systemName: "person.fill")
+                        TextField("Your Account", text: $account, onEditingChanged: getFocus1)
+                            .accentColor(.blue)
+                            .frame(height: 38)
+                    }
+                    Rectangle()
+                        .frame( height: 3 )
+                        .foregroundColor(isClicked1 ? .blue : .gray)
                 }
+                .shadow(radius: 10)
+                .padding(.horizontal,40)
+                
+                VStack(){
+                    HStack(){
+                        Image(systemName: "lock.fill")
+                        SecureField("Your Password", text: $password)
+                            .accentColor(.blue)
+                            .frame(height: 38)
+                            .onTapGesture {
+                                isClicked2 = true
+                            }
+                            .onSubmit {
+                                isClicked2 = false
+                            }
+                    }
+                    Rectangle()
+                        .frame( height: 3 )
+                        .foregroundColor(isClicked2 ? .blue : .gray)
+                }
+                .shadow(radius: 10)
+                .padding(.horizontal,40)
+                .padding(.vertical)
+                NavigationLink(destination: account == trueaccount && password == truepassword
+                               ? Text("登录成功").font(.largeTitle).fontWeight(.heavy)
+                               : Text("登录失败").font(.largeTitle).fontWeight(.heavy).foregroundColor(.red)){
+                    Text("登录")
+                        .frame(width: 80, height: 30 )
+                        .background(.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(15)
+                }
+                .shadow( radius: 10, y: 10)
+                .padding(.vertical, 50)
+                Spacer()
+                Link("清水河畔官网", destination: URL(string: "https://bbs.uestc.edu.cn")!)
             }
         }
     }
+    
+    func getFocus1(focused:Bool) {
+            isClicked1 = focused
+        }
 }
- 
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
 
 
 
